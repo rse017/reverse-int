88a7645e5edda9d268a2c1356f58f751f9ead0c8
Sergei R <rse017@gmail.com>
2022-08-11 11:03:27 +0300

    feat: solved the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+
+const a = Math.abs(n)
+const b = a.toString()
+const c = b.split('')
+const d = c.reverse()
+const e = d.join('')
+const f = +e
+return f;
+
 }
